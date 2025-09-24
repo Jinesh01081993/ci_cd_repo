345b8c401a131518bde8da97daee4dc207a66e55
Jinesh01081993 <125266384+Jinesh01081993@users.noreply.github.com>
2025-09-24 18:10:14 +0530

    Update before_script.sh

--- before_script.sh
+++ before_script.sh
@@ -1,1 +1,8 @@
-rm /var/www/html/index.html
+#!/bin/bash
+echo "Running before install script..."
+# Remove default index.html safely
+rm -f /var/www/html/index.html
+
+# Any other setup commands
+apt-get update
+apt-get install -y curl nginx
